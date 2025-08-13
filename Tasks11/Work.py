0a50b52cf0d5f99f74688bd96c2e450ff789b33c
NikSigma <mykolakotkov1@gmail.com>
2025-08-13 19:28:25 +0300

    пон

---
+++ Tasks11/Work.py
@@ -0,0 +1,25 @@
+killers = ["Bluedude", "c00lkid", "007n7", "1x1x1x1", "Jason", "John Doe"]
+
+print(killers)
+
+print("Noli: ДОДАЙТЕ МЕНЕ В ГРУ")
+
+killers.append("Noli")
+
+print("Додано Noli", killers)
+
+print("c00lkid: ЧОГО ЙОГО ДОДАЛИ?")
+print("bluedude: він добрий")
+print("c00lkid: CORUPT NATURE and BLUEDUDE")
+print("bluedude: АААААААААААААААААААААААААА")
+
+hiders = ("guest1337", "shedletsky", "eliot", "builderman", "Noob")
+
+print(
+
+
+"Shedletsky: СМАЖЕНА КУРКА")
+print("Guest1337: ДА НЕ КРИЧИ НАС ПОЧУЮТЬ")
+print("Shedletsky: АААААААААААААААААА Я ХОЧУ КУРКУ")
+print("Eliot: Я ДАМ ТОБІ ПІЦУ")
+print("Shedletsky: АААААААААААААААААА Я ХОЧУ КУРКУ")
