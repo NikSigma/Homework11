c4c9a76d04edaf9f46f52052443db798d673e5b0
NikSigma <mykolakotkov1@gmail.com>
2025-08-24 20:16:46 +0300

    rework

--- Tasks11/homework2.py
+++ Tasks11/homework2.py
@@ -1,9 +1,12 @@
-books = ["Harry Potter-1997", "Forest's kids-2020", "Percy Jackson-2010", "Cats wariors-2001"]
+books = [("Harry Potter", 1997), ("Forest's kids", 2020), ("Percy Jackson", 2010), ("Cats wariors", 2001)]
+
 print(books)
-books.append("Finista-2022")
-books.append("Lunastry-2015")
-books.append("Часодії-2011")
-books.remove("Harry Potter-1997")
+
+books.append(("Finista", 2022))
+books.append(("Lunastry", 2015))
+books.append(("Часодії", 2011))
+books.remove(("Harry Potter", 1997))
+
 print(books)
 
 
